f417c91c5cedda724463cc02599302732670059f
Marcos Salto <marcosasalto@gmail.com>
2024-08-26 16:02:30 -0300

    Create packages and constants

---
+++ app/src/main/java/com/marcossalto/emailapp/core/Constants.kt
@@ -0,0 +1,8 @@
+package com.marcossalto.emailapp.core
+
+class Constants {
+    companion object {
+        const val DATABASE_NAME = "mail_db"
+        const val MAIL_TABLE = "mail_table"
+    }
+}
